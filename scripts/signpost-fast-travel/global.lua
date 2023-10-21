596efae3f60f7e1c32f0b6ce5a6777d905d8346b
Hristos N. Triantafillou <me@hristos.triantafillou.us>
2023-10-21 12:06:09 -0500

    Time cost now works

--- scripts/signpost-fast-travel/global.lua
+++ scripts/signpost-fast-travel/global.lua
@@ -1,5 +1,4 @@
 require("scripts.signpost-fast-travel.checks")
-local async = require('openmw.async')
 local core = require('openmw.core')
 local types = require('openmw.types')
 local util = require('openmw.util')
@@ -10,41 +9,26 @@ local signs = require("scripts.signpost-fast-travel.signs")
 local function doTeleport(data)
     local playerPos = data.actor.position
     local targetPos = data.pos
-    local timeMachine = world.createObject("momw_sft_timemachine")
 
     -- https://wiki.openmw.org/index.php?title=Research:Trading_and_Services#Travel_costs
     local distance = (
         math.sqrt((playerPos.x - targetPos.x) * (playerPos.x - targetPos.x)
             + (playerPos.y - targetPos.y) * (playerPos.y - targetPos.y))
     )
+
     distance = distance / core.getGMST("fTravelTimeMult")
-    local gameHour = core.getGameTime() / 60 / 60 - 24 / 24 % 24
-    gameHour = gameHour + distance
-    timeMachine:teleport(data.actor.cell, data.actor.position)
-
-    -- A one frame delay is needed before things that are created can be used
-    async:newUnsavableSimulationTimer(
-        0.1,
-        function()
-            world.mwscript.getLocalScript(timeMachine).variables.newHour = gameHour
-            timeMachine:remove(1)
-
-            --TODO: Teleport followers; attendme does this
-            --TODO: Random chance to stop midway and spawn enemies
-            --TODO: Make the destination a random point around the signpost if no explicit location is found
-            data.actor:sendEvent(
-                "momw_sft_announceTeleport",
-                {
-                    hours = gameHour,
-                    name = data.cell.name
-                }
-            )
-            data.actor:teleport(
-                data.cell.name,
-                util.vector3(targetPos.x, targetPos.y, targetPos.z),
-                data.actor.rotation--TODO: set a fixed rotation for each target!
-            )
-        end
+    world.mwscript.getGlobalScript("momw_sft_timemachine_scr").variables.distance = distance
+    data.actor:sendEvent(
+        "momw_sft_announceTeleport",
+        {
+            hours = distance,
+            name = data.cell.name
+        }
+    )
+    data.actor:teleport(
+        data.cell.name,
+        util.vector3(targetPos.x, targetPos.y, targetPos.z),
+        data.actor.rotation--TODO: set a fixed rotation for each target!
     )
 end
 
